4c8946cf8b939fa3fac0d2534ca49e8eaa1a90f6
alecs <lecs.dumitru@gmail.com>
2014-11-07 01:45:31 +0000

    reading old files

---
+++ aco_portfolio/solution/classes/aco.py
@@ -0,0 +1,30 @@
+import networkx as nx 
+import abc
+from solution import Solution
+from ant import Ant
+
+class ACO():
+	__metaclass__ = abc.ABCMeta
+
+	alpha = 1 
+	betha = 3
+	p = 0.9
+
+	def __init__(self):
+		self.G = None
+		self.best_solution = Solution()
+		self.ants = []
+		
+
+
+	@abc.abstractmethod
+	def initialize_pheromones(self):
+		return
+
+	@abc.abstractmethod
+	def initialize_ants(self,number):
+		return
+
+	@abc.abstractmethod
+	def run(self, iterations = 1000, ants_no = 100):
+		return
---
+++ aco_portfolio/solution/classes/ant.py
@@ -0,0 +1,50 @@
+from solution import Solution
+from sets import Set
+from time_window import TimeWindow
+
+
+class Ant():
+
+    ant_id = 0
+
+    def __init__(self, start, end, graph):
+        self.solution = Solution()
+        self.curr_node = None
+        self.ant_id = Ant.ant_id
+        Ant.ant_id += 1
+
+        self.graph = graph
+        self.unavailable = Set([])
+        self.total_weight = 0
+        self.time =  0;
+        self.rr = 0
+        
+
+        self.capital = 25000
+        self.generated = 0
+        self.move_next(start)
+
+    def move_next(self, node):
+        self.solution.update_path(node)
+        self.curr_node = node
+        self.total_weight += self.graph.node[self.curr_node]['cost']
+
+    def get_weight(self):
+        return self.total_weight
+
+    def get_neighbours(self):
+        neigh = self.graph.neighbors(self.curr_node)
+        sanitized = []
+        self.rr += 3
+
+        for x in neigh:
+            if x not in self.solution.path and x not in self.unavailable:
+                #TEST FOR EMPTY NODE{X} => node[x] = {}
+
+                if self.ant_id ==0 :
+                    pass
+                    #print str(x) + "cost: " + str(self.graph.node[x]['cost']) + " -- weight:" + str(self.total_weight)
+
+                if self.graph.node[x]['cost'] + self.total_weight <= self.capital + self.generated:
+                    sanitized.append(x)
+        return sanitized
