497918235b16cd4a6239a856bbad54c43ddc58c4
alecs <lecs.dumitru@gmail.com>
2015-01-29 19:38:28 +0000

    Heroku deploy again

--- aco_portfolio/settings.py
+++ aco_portfolio/settings.py
@@ -26,9 +26,6 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -85,12 +82,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     #'/static',
 )
 STATIC_URL = '/static/'
 STATIC_ROOT = "static_root"
+
+
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 
